6df106e70d61d37bed3845e4f6013caeb98b81e8
Safi Sohail Azim <safiazim8@gmail.com>
2021-04-02 12:23:35 +0500

    Using Custom Fonts

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
 
 import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
@@ -11,7 +14,29 @@ const rootReducer = combineReducers({
 
 const store = createStore(rootReducer);
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
+
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => {
+          setFontLoaded(true);
+        }}
+        onError={() => {
+          setFontLoaded(false);
+        }}
+      />
+    );
+  }
   return (
     <Provider store={store}>
       <ShopNavigator />
--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -77,10 +77,12 @@ const styles = StyleSheet.create({
     overflow: "hidden",
   },
   title: {
+    fontFamily: "open-sans-bold",
     fontSize: 18,
-    marginVertical: 4,
+    marginVertical: 2,
   },
   price: {
+    fontFamily: "open-sans",
     fontSize: 14,
     color: "#888",
   },

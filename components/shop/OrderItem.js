b2e2c0ac6e573cd435c36975d554fd3a0008be9d
Safi Sohail Azim <safiazim8@gmail.com>
2021-04-11 15:54:02 +0500

    Styling Order Items

---
+++ components/shop/OrderItem.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+import Colors from "../../constants/Colors";
+import CartItem from "./CartItem";
+
+const OrderItem = (props) => {
+  return (
+    <View style={styles.orderItem}>
+      <View style={styles.summary}>
+        <Text style={styles.totalAmount}>${props.amount.toFixed(2)}</Text>
+        <Text style={styles.date}>{props.date}</Text>
+      </View>
+      <Button color={Colors.primary} title="Show Details" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  orderItem: {
+    shadowColor: "black",
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    elevation: 5,
+    borderRadius: 10,
+    backgroundColor: "white",
+    margin: 20,
+    padding: 10,
+    alignItems: "center",
+  },
+  summary: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    marginBottom: 15,
+  },
+  totalAmount: {
+    fontFamily: "open-sans-bold",
+    fontSize: 15,
+  },
+  date: {
+    fontSize: 16,
+    fontFamily: "open-sans",
+    color: "#888",
+  },
+});
+
+export default OrderItem;

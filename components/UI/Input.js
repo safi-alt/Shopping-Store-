56f4a59a1846aa5d651a1a18b1a76f608ccac94d
Safi Sohail Azim <safiazim8@gmail.com>
2021-04-19 16:18:14 +0500

    Connecting Input Component and Form

--- components/UI/Input.js
+++ components/UI/Input.js
@@ -1,11 +1,22 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect } from "react";
 import { View, Text, TextInput, StyleSheet } from "react-native";
 
 const INPUT_CHANGE = "INPUT_CHANGE";
+const INPUT_BLUR = "INPUT_BLUR";
 
 const inputReducer = (state, action) => {
   switch (action.type) {
     case INPUT_CHANGE:
+      return {
+        ...state,
+        value: action.value,
+        isValid: action.isValid,
+      };
+    case INPUT_BLUR:
+      return {
+        ...state,
+        touched: true,
+      };
 
     default:
       return state;
@@ -18,6 +29,15 @@ const Input = (props) => {
     isValid: props.initiallyValid,
     touched: false,
   });
+
+  const { onInputChange, id } = props;
+
+  useEffect(() => {
+    if (inputState.touched) {
+      onInputChange(id, inputState.value, inputState.isValid);
+    }
+  }, [inputState, onInputChange, id]);
+
   const textChangeHandler = (text) => {
     const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     let isValid = true;
@@ -36,7 +56,11 @@ const Input = (props) => {
     if (props.minLength != null && text.length < props.minLength) {
       isValid = false;
     }
-    dispatch({ type: INPUT_CHANGE, value: text });
+    dispatch({ type: INPUT_CHANGE, value: text, isValid: isValid });
+  };
+
+  const lostFocusHandler = () => {
+    dispatch({ type: INPUT_BLUR });
   };
 
   return (
@@ -45,10 +69,11 @@ const Input = (props) => {
       <TextInput
         {...props}
         style={styles.input}
-        value={formState.inputValues.title}
-        onChangeText={textChangeHandler.bind(this, "title")}
+        value={inputState.value}
+        onChangeText={textChangeHandler}
+        onBlur={lostFocusHandler}
       />
-      {!formState.inputValidities.title && <Text>{props.errorText}</Text>}
+      {!inputState.isValid && <Text>{props.errorText}</Text>}
     </View>
   );
 };

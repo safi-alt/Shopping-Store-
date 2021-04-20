1026ec78ca7f248e41975efec2b5652b486a7417
Safi Sohail Azim <safiazim8@gmail.com>
2021-04-20 17:12:22 +0500

    Storing Products on a Server

--- store/actions/products.js
+++ store/actions/products.js
@@ -7,14 +7,36 @@ export const deleteProduct = (productId) => {
 };
 
 export const createProduct = (title, description, imageUrl, price) => {
-  return {
-    type: CREATE_PRODUCT,
-    productData: {
-      title,
-      description,
-      imageUrl,
-      price,
-    },
+  return async (dispatch) => {
+    // any async code you want !
+    const response = await fetch(
+      "https://rn-complete-guide-61a84-default-rtdb.firebaseio.com/products.json",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title,
+          description,
+          imageUrl,
+          price,
+        }),
+      }
+    );
+
+    const resData = await response.json();
+
+    dispatch({
+      type: CREATE_PRODUCT,
+      productData: {
+        id: resData.name,
+        title,
+        description,
+        imageUrl,
+        price,
+      },
+    });
   };
 };
 export const updateProduct = (id, title, description, imageUrl) => {

eef74d8077daa370ef6fecd45666490708d58442
Safi Sohail Azim <safiazim8@gmail.com>
2021-04-24 23:48:17 +0500

    Fetching Stored Orders

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,4 +1,42 @@
 export const ADD_ORDER = "ADD_ORDER";
+import Order from "../../models/order";
+export const SET_ORDERS = "SET_ORDERS";
+
+export const fetchOrders = () => {
+  return async (dispatch) => {
+    try {
+      const response = await fetch(
+        "https://rn-complete-guide-61a84-default-rtdb.firebaseio.com/orders/u1.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const resData = await response.json();
+      const loadedOrders = [];
+
+      for (const key in resData) {
+        loadedOrders.push(
+          new Order(
+            key,
+            resData[key].cartItems,
+            resData[key].totalAmount,
+            new Date(resData[key].date)
+          )
+        );
+      }
+
+      dispatch({
+        type: SET_ORDERS,
+        orders: loadedOrders,
+      });
+    } catch (err) {
+      // send to custom analytics server
+      throw err;
+    }
+  };
+};
 
 export const addOrder = (cartItems, totalAmount) => {
   return async (dispatch) => {

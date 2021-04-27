6d0888fedbfa766bf1cb4a2307bb6c8e8ec3237b
Safi Sohail Azim <safiazim8@gmail.com>
2021-04-27 16:41:58 +0500

    Managing the Loading State and Errors

--- screens/user/AuthScreen.js
+++ screens/user/AuthScreen.js
@@ -1,10 +1,12 @@
-import React, { useReducer, useCallback, useState } from "react";
+import React, { useReducer, useCallback, useState, useEffect } from "react";
 import {
   ScrollView,
   StyleSheet,
   View,
   KeyboardAvoidingView,
   Button,
+  ActivityIndicator,
+  Alert,
 } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 
@@ -41,6 +43,8 @@ const formReducer = (state, action) => {
 };
 
 const AuthScreen = (props) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
   const dispatch = useDispatch();
   const [isSignup, setIsSignup] = useState(false);
 
@@ -56,7 +60,13 @@ const AuthScreen = (props) => {
     formIsValid: false,
   });
 
-  const authHandler = () => {
+  useEffect(() => {
+    if (error) {
+      Alert.alert("An Error Occurred", error, [{ text: "Okay" }]);
+    }
+  }, [error]);
+
+  const authHandler = async () => {
     let action;
     if (isSignup) {
       action = authActions.signup(
@@ -69,7 +79,14 @@ const AuthScreen = (props) => {
         formState.inputValues.password
       );
     }
-    dispatch(action);
+    setError(null);
+    setIsLoading(true);
+    try {
+      await dispatch(action);
+    } catch (err) {
+      setError(err.message);
+    }
+    setIsLoading(false);
   };
 
   const inputChangeHandler = useCallback(
@@ -85,8 +102,8 @@ const AuthScreen = (props) => {
   );
   return (
     <KeyboardAvoidingView
-      behavior="height"
-      keyboardVerticalOffset={50}
+      // behavior="height"
+      // keyboardVerticalOffset={50}
       style={styles.screen}
     >
       <LinearGradient colors={["#ffedff", "#ffe3ff"]} style={styles.gradient}>
@@ -116,11 +133,15 @@ const AuthScreen = (props) => {
               initialValue=""
             />
             <View style={styles.buttonContainer}>
-              <Button
-                title={isSignup ? "Sign Up" : "Login"}
-                color={Colors.primary}
-                onPress={authHandler}
-              />
+              {isLoading ? (
+                <ActivityIndicator size="small" color={Colors.primary} />
+              ) : (
+                <Button
+                  title={isSignup ? "Sign Up" : "Login"}
+                  color={Colors.primary}
+                  onPress={authHandler}
+                />
+              )}
             </View>
             <View style={styles.buttonContainer}>
               <Button
--- store/actions/auth.js
+++ store/actions/auth.js
@@ -19,7 +19,16 @@ export const signup = (email, password) => {
     );
 
     if (!response.ok) {
-      throw new Error("Something went Wrong");
+      const errorResData = await response.json();
+      //console.log(errorResData);
+      const errorId = errorResData.error.message;
+
+      let message = "Something Went wrong";
+      if (errorId === "EMAIL_EXISTS") {
+        message = "This email exists already";
+      }
+
+      throw new Error(message);
     }
 
     const resData = await response.json();
@@ -48,7 +57,18 @@ export const login = (email, password) => {
     );
 
     if (!response.ok) {
-      throw new Error("Something went Wrong");
+      const errorResData = await response.json();
+      //console.log(errorResData);
+      const errorId = errorResData.error.message;
+
+      let message = "Something Went wrong";
+      if (errorId === "EMAIL_NOT_FOUND") {
+        message = "This email could not be found";
+      } else if (errorId === "INVALID_PASSWORD") {
+        message = "This password is not valid";
+      }
+
+      throw new Error(message);
     }
 
     const resData = await response.json();

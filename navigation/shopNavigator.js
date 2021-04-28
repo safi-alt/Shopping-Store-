f2e97e26ac4fe68ed8a4de3ca3ea8925ae4e12a8
Safi Sohail Azim <safiazim8@gmail.com>
2021-04-28 17:54:32 +0500

    Implementing Auto Login

--- navigation/shopNavigator.js
+++ navigation/shopNavigator.js
@@ -13,6 +13,7 @@ import OrdersScreen from "../screens/shop/OrdersScreen";
 import UserProductsScreen from "../screens/user/UserProductsScreen";
 import EditProductScreen from "../screens/user/EditProductScreen";
 import AuthScreen from "../screens/user/AuthScreen";
+import StartupScreen from "../screens/StartupScreen";
 
 import { Ionicons } from "@expo/vector-icons";
 
@@ -108,6 +109,7 @@ const AuthNavigator = createStackNavigator(
 );
 
 const MainNavigator = createSwitchNavigator({
+  Startup: StartupScreen,
   Auth: AuthNavigator,
   Shop: ShopNavigator,
 });
